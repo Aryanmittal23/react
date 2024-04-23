2ccc4435fac37c2b5a4e7fd2aea4f24d628784a1
Aryanmittal23 <mittalaryan932@gmail.com>
2024-04-24 02:47:37 +0530

    react

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './component/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username:"Aryan",
+    age:21
+  }
+  let newArr = [1,2,3]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+
+      <Card username="CodeWithAryan" />
+      <Card username="me"/>
+
+    </>
+  )
+
+}
+export default App
